7928c524e6f85fafca5f0e60a38f00e4ed9b475e
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-04-24 20:45:25 +0200

    UPDATE: Webpack structure on folders and files

---
+++ SecondVersion/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/javascript/script.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
+  },
+  devServer: { //Fpr the development server
+    static: {
+      directory: path.join(__dirname), // Here we define where is the index.html
+    },
+    port: 9000, // which port we want to use
+  },
+};
