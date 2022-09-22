f4193e428699601d8953d108443ff042de1d9d28
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-09-22 22:34:12 +0200

    ADD: logo and contact list

---
+++ third-version-vue/src/utils/contacts.js
@@ -0,0 +1,63 @@
+export default 
+[
+  { 'name' : 'Kiss Bence',
+    'phone': '3670303030',
+    'email': 'bence.kiss@vodafone.com',
+    'showing': true  
+  },
+  { 'name' : 'Kovacs Ibolya',
+    'phone': '3670404040',
+    'email': 'ibolya.kovacs@vodafone.com',
+    'showing': true
+  },
+  { 'name' : 'Nagy David',
+    'phone': '3670505050',
+    'email': 'david.nagy@vodafone.com',
+    'showing': true
+  },
+  { 'name' : 'Apro Balint',
+    'phone': '3670606060',
+    'email': 'balint.apro@vodafone.com',
+    'showing': true
+  },
+  { 'name' : 'Ernesto Tzompantzi',
+    'phone': '3670707070',
+    'email': 'tzompantzi.ernesto@vodafone.com',
+    'showing': true
+  },
+  {'name' : 'Nagy Renata',
+  'phone': '3670808080',
+  'email': 'renata.nagy@vodafone.com',
+  'showing': true
+  },
+  { 'name' : 'David Zoltan',
+    'phone': '3670909090',
+    'email': 'zoltan.david@vodafone.com',
+    'showing': true
+  },
+  {'name' : 'Toth Tomas',
+  'phone': '3671010100',
+  'email': 'tomas.toth@vodafone.com',
+  'showing': true
+  },
+  {'name' : 'Fodor Bernadett',
+  'phone': '3671111110',
+  'email': 'bernadett.fodor@vodafone.com',
+  'showing': true
+  },
+  {'name' : 'Arnold Bath',
+  'phone': '3671103010',
+  'email': 'arnold.bath@vodafone.com',
+  'showing': true
+  },
+  {'name' : 'Jones Chris',
+  'phone': '36712223010',
+  'email': 'jones.chris@vodafone.com',
+  'showing': true
+  },
+  {'name' : 'Kuz Nate',
+  'phone': '36712943810',
+  'email': 'kuz.nate@vodafone.com',
+  'showing': true
+  },
+];
