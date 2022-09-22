5a813128ecfbac3ab0d1592a75d9e242f3723c89
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-09-22 22:35:05 +0200

    UPDATE: App.vue and main.js

---
+++ third-version-vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <title-bar></title-bar>
+    <contacts-stored></contacts-stored>
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TitleBar from "./components/TitleBar.vue";
+import ContactsStored from './components/ContactsStored.vue';
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    TitleBar,
+    ContactsStored,
+    TheFooter
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  background-color: white;
+  margin: 0;
+}
+</style>
