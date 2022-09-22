8670fd88a3127af9408dff3ea872062333393cf6
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-09-22 22:34:56 +0200

    ADD: TitleBar and Contacts Stored components

---
+++ third-version-vue/src/components/ContactsStored.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+  </ul>
+</template>
+
+<script>
+import tempContacts from "../utils/contacts.js";
+
+export default {
+  data() {
+    return {
+      contacts: tempContacts,
+    };
+  },
+  methods: {
+    showCont() {
+      this.contacts.forEach((e) => {
+        console.log(e.name);
+      });
+    },
+  },
+  mounted() {
+    this.showCont();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ third-version-vue/src/components/TitleBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="col-md-12 text-center">
+      <div id="header-title">Contacts</div>
+    </div>
+    <div class="underline-contacts"></div>
+    <div class="col-md-12 filter-table">
+      <div class="filter-element">A</div>
+      <div class="filter-element">B</div>
+      <div class="filter-element">C</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#header-title {
+  font-size: 40px;
+  font-weight: 600;
+}
+.underline-contacts {
+  content: "";
+  height: 3px;
+  width: 5%;
+  background-color: red;
+  margin-left: 48%;
+}
+.filter-table {
+  font-size: 30px;
+  margin-top: 10px;
+  padding-left: 35%;
+  font-weight: 400;
+  visibility: visible;
+  display: flex;
+}
+.filter-element {
+  margin-right: 3rem;
+  cursor: pointer;
+}
+</style>
