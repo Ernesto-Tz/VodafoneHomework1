a72fceee43191e2515eaa0d799eb2172446a8c18
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-09-22 22:34:31 +0200

    ADD: Footer and Header components

---
+++ third-version-vue/src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="container">
+      <p>All rights Reserved. VSSB 2018</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer{
+  position: relative;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  margin-top: 50px;
+  text-align: center;
+  font-size: medium;
+  padding-top: 20px;
+}
+</style>
---
+++ third-version-vue/src/components/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="row">
+    <div class="col-md-6">
+      <a href="#">
+        <img
+          class="logo"
+          src="../assets/vodafone_logo.svg"
+          alt="vodafone_logo"
+        />
+      </a>
+    </div>
+    <div class="col-md-6 float-right">
+      <input
+        class="form-control"
+        type="text"
+        name="search"
+        placeholder="Search for name"
+      />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 2rem;
+  width: 100%;
+  max-height: 150px;
+}
+
+@media only screen and (max-width: 600px) {
+  .logo {
+    width: 150px;
+    height: 60px;
+    position: relative;
+    left: 40px;
+    bottom: 5px;
+  }
+}
+
+.logo {
+  width: 250px;
+  height: 100px;
+  position: relative;
+  left: 40px;
+  bottom: 5px;
+}
+
+input {
+  margin-top: 5%;
+  margin-right: 10%;
+  margin-bottom: 15px;
+  width: 75%;
+  /* padding: 8px; */
+  /* border-radius: 5px; */
+  /* position: relative; */
+  /* border-width: thin; */
+}
+</style>
