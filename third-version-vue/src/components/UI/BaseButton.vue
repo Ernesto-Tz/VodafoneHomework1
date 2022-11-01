b013e00884e0e0b815d54c8ad66cf311518835eb
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-11-01 19:25:36 +0100

    ADD: Delete modal and base button elements

---
+++ third-version-vue/src/components/UI/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :type="type" :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+};
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  margin: 0.5rem;
+  font-family: inherit;
+  background-color: #E60000;
+  border: 1px solid #E60000;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #800020;
+  border-color: #800020;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
--- third-version-vue/src/main.js
+++ third-version-vue/src/main.js
@@ -7,16 +7,22 @@ import "bootstrap";
 import ContactsList from './components/ContactsList.vue'
 import ContactDetails from './components/ContactDetails.vue'
 import ContactEdit from './components/ContactEdit.vue'
+import AddContact from './components/AddContact.vue'
+import ErrorAlert from './components/UI/ErrorAlert.vue'
+import BaseButton from './components/UI/BaseButton.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/contacts', component: ContactsList},
     { path: '/contacts/details/:contactId', component: ContactDetails, props: true},
-    { path: '/contacts/edit/:contactId', component: ContactEdit, props: true}
+    { path: '/contacts/edit/:contactId', component: ContactEdit, props: true},
+    { path: '/contacts/add', component: AddContact}
   ]
 });
 
 const app = createApp(App);
 app.use(router);
+app.component('base-button', BaseButton);
+app.component('error-alert', ErrorAlert);
 app.mount('#app');
