48a3cccdc9f2dea737ae026f79bd8fdc5eaeb4c6
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2022-11-02 21:29:10 +0100

    UPDATE: routing for catching other routes

---
+++ third-version-vue/src/components/UI/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h2>Page not Found go back <router-link to="/contacts">Home</router-link></h2>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  margin: 5rem auto;
+}
+</style>
--- third-version-vue/src/main.js
+++ third-version-vue/src/main.js
@@ -10,14 +10,17 @@ import ContactEdit from './components/ContactEdit.vue'
 import AddContact from './components/AddContact.vue'
 import ErrorAlert from './components/UI/ErrorAlert.vue'
 import BaseButton from './components/UI/BaseButton.vue'
+import NotFound from './components/UI/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/contacts'},
     { path: '/contacts', component: ContactsList},
     { path: '/contacts/details/:contactId', component: ContactDetails, props: true},
     { path: '/contacts/edit/:contactId', component: ContactEdit, props: true},
-    { path: '/contacts/add', component: AddContact}
+    { path: '/contacts/add', component: AddContact},
+    { path: '/:notFound(.*)', component: NotFound},
   ]
 });
 
