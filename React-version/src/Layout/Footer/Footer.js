be7f48431052e770899c401d309ecbaf57d06589
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-02-28 16:34:22 +0100

    ADD: Footer and Header

---
+++ React-version/src/Layout/Footer/Footer.js
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css'
+
+const Footer = props => {
+  return(
+    <footer className={classes.footer}>
+      <p>All rights Reserved <b>_VO</b>IS 2023</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ React-version/src/Layout/Header/Header.js
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+
+import Input from "../../UI/Input";
+import classes from "./Header.module.css";
+import VFlogo from "../../Assets/vodafone_logo.svg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={`row m-0 ${classes.header}`}>
+        <div className="col-md-6">
+          <img className={classes.logo} src={VFlogo} alt="vodafone_logo" />
+        </div>
+        <div className="col-md-6">
+          <Input
+            input={{
+              type: "text",
+              name: "search",
+              placeholder: "Search for contact",
+            }}
+          />
+        </div>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
