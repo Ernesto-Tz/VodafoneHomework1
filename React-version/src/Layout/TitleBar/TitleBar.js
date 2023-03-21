e06ab4a05c5afc4474d8ebfb8f5edab9e6d85737
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-03-21 17:59:34 +0100

    FIX: Display logic for the Title Bar elements

--- React-version/src/Layout/TitleBar/TitleBar.js
+++ React-version/src/Layout/TitleBar/TitleBar.js
@@ -7,41 +7,28 @@ const TitleBar = () => {
   const contactsCtx = useContext(ContactsContext);
 
   useEffect(() => {
-    // *** TODO: Fix this logic - pushing all elements.
-    const createFilter = () => {
-      for (const contact of contactsCtx.contacts) {
-        // if (lettersArray.length === 0) {
-        //   const newVal = contact.name[0].toUpperCase();
-        //   console.log(newVal);
-        //   setLettersArray((prevState) => {
-        //     return [...prevState, newVal];
-        //   });
-        // }
-        if (!lettersArray.includes(contact.name[0].toUpperCase())) {
-          const newVal = contact.name[0].toUpperCase();
-          console.log(newVal);
-          setLettersArray((prevState) => {
-            return [...prevState, newVal];
-          });
-        }
-        console.log(lettersArray);
+    let tempArr = [];
+    for (const contact of contactsCtx.contacts) {
+      if (!tempArr.includes(contact.name[0].toUpperCase())) {
+        const newVal = contact.name[0].toUpperCase();
+        tempArr.push(newVal);
       }
-    };
-    createFilter();
-  }, [contactsCtx.contacts, lettersArray]);
+    }
+    tempArr.sort();
+    setLettersArray((prevState) => {
+      return [...tempArr];
+    });
+  }, [contactsCtx.contacts]);
 
-  lettersArray.sort();
-
-  console.log(lettersArray);
   return (
     <div className={`${styles.container}`}>
       <div className={`${styles.title} text-center`}>
         <h3>Contacts</h3>
       </div>
       <div className={`${styles["underline-contacts"]}`} />
-      <div className={styles['filter-table']}>
-        {lettersArray.map((item) => (
-          <div className={styles['filter-element']}>{item}</div>
+      <div className={styles["filter-table"]}>
+        {lettersArray.map((item, index) => (
+          <div className={styles["filter-element"]} key={index}>{item}</div>
         ))}
       </div>
     </div>
