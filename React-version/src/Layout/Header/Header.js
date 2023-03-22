1b6f53779059772f0e22810e02ca6e1633194de6
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-03-22 17:18:47 +0100

    UPDATE: Header styles

--- React-version/src/Layout/Header/Header.js
+++ React-version/src/Layout/Header/Header.js
@@ -4,14 +4,14 @@ import Input from "../../UI/Input";
 import classes from "./Header.module.css";
 import VFlogo from "../../Assets/vodafone_logo.svg";
 
-const Header = (props) => {
+const Header = () => {
   return (
     <Fragment>
       <header className={`row m-0 ${classes.header}`}>
         <div className="col-md-6">
           <img className={classes.logo} src={VFlogo} alt="vodafone_logo" />
         </div>
-        <div className="col-md-6">
+        <div className="col-md-6 mt-3">
           <Input
             input={{
               type: "text",
