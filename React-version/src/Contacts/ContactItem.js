b9b1ec32da1e0db165f39fdbc8808f9f9773f14d
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-03-22 17:18:16 +0100

    ADD: Display conditionally contact item

--- React-version/src/Contacts/ContactItem.js
+++ React-version/src/Contacts/ContactItem.js
@@ -1,28 +1,29 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
 import ContactsContext from "../Store/contacts-context.js";
-import classes from './ContactItem.module.css'
+import classes from "./ContactItem.module.css";
 
-const ContactItem = props => {
-
+const ContactItem = (props) => {
   const contCtx = useContext(ContactsContext);
 
   const contactViewHandler = () => {
-    contCtx.displayItem({...props.contact});
+    contCtx.displayItem({ ...props.contact });
     props.onClick();
-  }
+  };
 
-  return(
-    <div className={`col-lg-4 col-md-6 ${classes.contact}`}>
-      <div className={classes['contact-name']} onClick={contactViewHandler}>
-        {props.contact.name}
+  return (
+    props.contact.showing && (
+      <div className={`col-lg-4 col-md-6 ${classes.contact}`}>
+        <div className={classes["contact-name"]} onClick={contactViewHandler}>
+          {props.contact.name}
+        </div>
+        <div className={classes["contact-info"]}>
+          <p>{props.contact.email}</p>
+          <p>{props.contact.phone}</p>
+        </div>
       </div>
-      <div className={classes['contact-info']}>
-        <p>{props.contact.email}</p>
-        <p>{props.contact.phone}</p>
-      </div>
-    </div>
+    )
   );
-}
+};
 
 export default ContactItem;
